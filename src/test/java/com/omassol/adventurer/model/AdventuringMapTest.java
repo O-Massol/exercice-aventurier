9dc03684af28ce8976a63a9640666a3693239f85
Olivier Massol <o.massol@interpol.int>
2024-07-27 13:56:41 +0200

    NO-TICKET Imagine next step and refactor test naming

--- src/test/java/com/omassol/adventurer/model/AdventuringMapTest.java
+++ src/test/java/com/omassol/adventurer/model/AdventuringMapTest.java
@@ -4,15 +4,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.omassol.adventurer.support.StaticMapProvider;
 import com.omassol.adventurer.support.StaticPlannedTravelProvider;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AdventuringMapTest {
 
     @Test
-    void proceed_withSmallestMapAndSmallestPlannedTravel_positionReturned(){
+    @DisplayName("proceed ; with (1,1) map and (0,0,[]) planned travel ; (0,0) position returned")
+    void proceedSimplest(){
         var map = new StaticMapProvider().smallestMap();
         var position = map.proceed(new StaticPlannedTravelProvider().smallestTravel(new Position(0,0)));
         assertThat(position).isEqualTo(new Position(0,0));
     }
+
+//    @Test
+//    @DisplayName("proceed ; with (2,2) map and (0,0,[S]) planned travel ; (0,1) position returned")
+//    void proceedOneMove(){
+//    }
 
 }
